0050bac0fae7888df973851b0ef747f23af7b083
UmakantKulkarni <UmakantKulkarni@users.noreply.github.com>
2025-02-21 17:30:27 -0500

    Use all dataset

--- src/llama_cookbook/configs/datasets.py
+++ src/llama_cookbook/configs/datasets.py
@@ -42,6 +42,6 @@ class llamaguard_toxicchat_dataset:
 @dataclass
 class open5gs_dataset:
     dataset: str = "open5gs_dataset"
-    train_split: str = "train[:1%]"
-    val_split: str = "train[1%:2%]"
-    test_split: str = "train[2%:3%]"
+    train_split: str = "train[:70%]"
+    val_split: str = "train[70%:85%]"
+    test_split: str = "train[85%:100%]"
